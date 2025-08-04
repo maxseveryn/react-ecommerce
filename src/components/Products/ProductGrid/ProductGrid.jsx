4147c401e8a0e498c2a8841fd205648ef6458fa0
maxseveryn <severinmaksim055@gmail.com>
2025-08-04 18:38:07 +0300

    Move products fetch logic from ProductGrid to Homepage component

--- src/components/Products/ProductGrid/ProductGrid.jsx
+++ src/components/Products/ProductGrid/ProductGrid.jsx
@@ -1,29 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ProductCard from "../Product/ProductCard.jsx";
 import "./ProductGrid.css";
 
 export default function ProductGrid(props) {
-  const { productsName } = props;
-  const [products, setProducts] = useState([]);
-
+  const { productsName, products } = props;
   const [maxItems, setMaxItems] = useState(12);
 
   const handleLoadMore = () => {
     setMaxItems((prev) => prev + maxItems);
   };
 
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((err) => console.error("Error fetching products", err));
-  }, []);
-
   return (
     <div className="products-container">
       <h1 className="products__name">{productsName}VIP</h1>
       <div className="products__grid" role="list" tabIndex="-1">
-        {products.slice(0, maxItems).map((product) => (
+        {products?.slice(0, maxItems).map((product) => (
           <ProductCard key={product.id} data={product} />
         ))}
       </div>
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Homepage.css";
-import NavBar from "../../components/NavBar/NavBar";
-import ImageSlider from "../../components/ImageSlider/ImageSlider";
-import ProductGrid from "../../components/Products/ProductGrid/ProductGrid";
+import NavBar from "../../components/NavBar/NavBar.jsx";
+import ImageSlider from "../../components/ImageSlider/ImageSlider.jsx";
+import ProductGrid from "../../components/Products/ProductGrid/ProductGrid.jsx";
 
 export default function Homepage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error("Error fetching products", err));
+  }, []);
+
   return (
     <div className="homepage">
       <title>Clothing Shop | Home</title>
       <NavBar />
       <div className="page-content">
         <ImageSlider />
-        <ProductGrid />
+        <ProductGrid products={products} />
       </div>
     </div>
   );
